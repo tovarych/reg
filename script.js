6dc7abf49fe25db64aaf7bff2efbb0a1a8fbd74b
tovarych <sie8igor@gmail.com>
2024-12-30 14:04:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ form.addEventListener('submit', (event) => {
     const formData = new FormData(form);
     const data = {};
 
+    // Сбор данных формы в объект
     formData.forEach((value, key) => {
         data[key] = value;
     });
 
-    tg.sendData(JSON.stringify(data)); // Отправляем данные в Telegram Bot
+    // Добавляем в данные новый ключ "type" с значением "shop"
+    data["type"] = "shop";
+
+    // Преобразуем данные в JSON строку
+    const jsonData = JSON.stringify(data);
+
+    try {
+        // Отправка данных в Telegram Bot
+        tg.sendData(jsonData);
+        console.log('Данные успешно отправлены:', jsonData);
+    } catch (error) {
+        // Обработка ошибок при отправке
+        console.error('Ошибка при отправке данных:', error);
+    }
 });
